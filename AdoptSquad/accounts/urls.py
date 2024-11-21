0e287550725a6ec3e798d91157796f2d5bcf6fe0
bojkata1 <bojidar.enchev03@abv.bg>
2024-11-21 14:54:15 +0200

    feat: created signup, login and logout functionalities

--- AdoptSquad/accounts/urls.py
+++ AdoptSquad/accounts/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.contrib.auth.views import LogoutView
+from django.urls import path, include
 
 from AdoptSquad.accounts.views import AppUserLoginView, AppUserRegisterView
 
 urlpatterns = [
-    path('accounts/', AppUserLoginView.as_view(), name='login'),
-    path('accounts/', AppUserRegisterView.as_view(), name='registration'),
+    path('login/', AppUserLoginView.as_view(), name='login'),
+    path('register/', AppUserRegisterView.as_view(), name='signup'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- AdoptSquad/accounts/views.py
+++ AdoptSquad/accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, get_backends
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -10,6 +10,20 @@ from AdoptSquad.accounts.models import AppUser
 class AppUserLoginView(LoginView):
     template_name = 'accounts/login.html'
 
+    def form_valid(self, form):
+        """Override form_valid to explicitly set the backend."""
+        user = form.get_user()
+
+        backend = next(
+            (backend for backend in get_backends() if hasattr(backend, 'authenticate')),
+            None
+        )
+        if backend:
+            user.backend = f"{backend.__module__}.{backend.__class__.__name__}"
+
+        login(self.request, user)
+        return super().form_valid(form)
+
 
 class AppUserRegisterView(CreateView):
     model = AppUser
@@ -20,6 +34,6 @@ class AppUserRegisterView(CreateView):
     def form_valid(self, form):
         response = super().form_valid(form)
 
-        login(request=self.request, user=self.object)
+        login(request=self.request, user=self.object, backend='AdoptSquad.accounts.backends.EmailOrUsernameBackend')
 
         return response
