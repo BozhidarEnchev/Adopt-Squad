373dd9f1da0f727ee7d26a442f25ecdf1e13e8e4
bojkata1 <bojidar.enchev03@abv.bg>
2024-12-03 20:16:19 +0200

    refactor: - renamed PublishableContentMixin to PublishableContentBase

--- AdoptSquad/forum/models.py
+++ AdoptSquad/forum/models.py
@@ -1,9 +1,28 @@
 from cloudinary.models import CloudinaryField
+from django.contrib.auth import get_user_model
 from django.db import models
-from AdoptSquad.forum.mixins import PublishableContentMixin
 
 
-class Post(PublishableContentMixin, models.Model):
+class PublishableContentBase(models.Model):
+    content = models.TextField(
+        max_length=1000,
+    )
+
+    author = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    time_of_publication = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Post(PublishableContentBase, models.Model):
     title = models.CharField(
         max_length=100,
     )
@@ -15,7 +34,7 @@ class Post(PublishableContentMixin, models.Model):
         return self.title
 
 
-class Comment(PublishableContentMixin, models.Model):
+class Comment(PublishableContentBase, models.Model):
     post = models.ForeignKey(
         to=Post,
         on_delete=models.CASCADE,
