7060c1e968dba1a1f2005fbd443862b332d0b326
bojkata1 <bojidar.enchev03@abv.bg>
2024-12-03 16:14:27 +0200

    feat: - created comment model - created mixin for comment and post models

--- AdoptSquad/forum/models.py
+++ AdoptSquad/forum/models.py
@@ -1,25 +1,23 @@
 from cloudinary.models import CloudinaryField
-from django.contrib.auth import get_user_model
 from django.db import models
+from AdoptSquad.forum.mixins import PublishableContentMixin
 
 
-class Post(models.Model):
+class Post(PublishableContentMixin, models.Model):
     title = models.CharField(
         max_length=100,
-    )
-
-    content = models.TextField(
-        max_length=1000,
     )
 
     image = CloudinaryField('image', blank=True, null=True, folder='adopt-squad/forum-post-pictures/',
                             format='png')
 
-    author = models.ForeignKey(
-        to=get_user_model(),
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-
     def __str__(self):
         return self.title
+
+
+class Comment(PublishableContentMixin, models.Model):
+    post = models.ForeignKey(
+        to=Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
